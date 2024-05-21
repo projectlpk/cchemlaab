b0b0a4915da1688a7d43dc88f3031fd80012d6e7
projectlpk <uliyakhoerunisa@gmail.com>
2024-05-21 23:12:48 +0700

    Add files via upload

---
+++ LPK/Home.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title="ChemLab",
+    page_icon="🥼",
+)
+
+st.sidebar.title("Pilih Halaman")
+
+st.title("CHEMISRY LAB 🥼🧪")
+st.subheader("Aplikasi ini di ciptakan untuk membantu penguna untuk memahami kimia")
+st.write('Aplikasi ini berisikan informasi mengenai unsur kimia dalam tabel periodik, membantu pengguna dalam menghitung berat molekul pada senyawa dan juga membantu dalam melakukan perhitungan titrimetri untuk mengurangi kesalah dalam penempatan rumus dan kesalahan perhitungan manual')
+
+st.write(' ')
+st.write('**OLEH KELOMPOK 4**')
+st.write('-Anjelita Putri Prasetya')
+st.write('-Fatimah Hikmah Syahidah')
+st.write('-Saskia Amanda Dwiputri')
+st.write('-Taqiyya Humaira')
+st.write('-Uliya Khoerunnisa')
